a2c8bdc905c50e4265a5144d636d72b2f8e0eca6
Owen Thompson <othomps1986@gmail.com>
2018-12-13 15:53:17 -0700

    Filters route clearing

--- routes/filters.js
+++ routes/filters.js
@@ -1,20 +1,9 @@
+
 'use strict'
 
 var express = require('express')
 var router = express.Router()
 let knex = require('../knex')
-
-/* GET home page. */
-
-router.get('/', (req, res, next) => {
-  knex('filters')
-    .then((filter) => {
-      res.send(filter)
-    })
-    .catch((err) => {
-      next(err)
-    })
-})
 
 router.post('/', (req, res, next) => {
   knex('filters')
@@ -23,17 +12,23 @@ router.post('/', (req, res, next) => {
     })
 })
 
+router.get('/', (req, res, next) => {
+  knex('filters')
+    .then(data => {
+      res.send(data)
+    })
+})
+
 router.delete('/filters/:id', (req, res, next) => {
   return knex('filters')
-    .del('*')
     .where('id', req.params.id)
+    .del('*')
     .then((filters) => {
       if (!filters.length) {
         return next()
       }
       const filter = filters[0]
-
-      delete filter.id
+      // delete filter.id
       res.send(filter)
     })
     .catch((err) => {
