8af5111dc0728ac3cf0ab61d6136b8ea5c1cee23
Cooper Lightfoot <cooperlightfoot@Coopers-MacBook-Air.local>
2018-12-17 10:29:28 -0700

    Mvp Completed

--- public/javascripts/feed.js
+++ public/javascripts/feed.js
@@ -3,6 +3,64 @@ let filterArray = []
 let filterList = document.querySelector('.filterList')
 
 document.addEventListener('DOMContentLoaded', () => {
+  const updateFeed = (userInfomation3) =>{
+    axios({
+      method: 'post',
+      url: '/news',
+      data: userInfomation3
+    })
+    .then((response) => {
+      list.innerHTML = ''
+      for (var i = 0; i < response.data.articles.length; i++) {
+        let newThread = document.createElement('li')
+        let newImage = document.createElement('img')
+        let newDiv = document.createElement('div')
+        let newHead = document.createElement('h5')
+        let newText = document.createElement('p')
+        let newLink = document.createElement('a')
+        if (response.data.articles[i].title) {
+          newHead.innerHTML = response.data.articles[i].title
+        } else {
+          newHead.innerHTML = 'No Headline provided'
+        }
+        if (response.data.articles[i].author) {
+          newHead.innerHTML += `, Created by: ${response.data.articles[i].author}`
+        }
+        if (response.data.articles[i].content) {
+          newText.innerHTML = response.data.articles[i].content
+        } else {
+          newText.innerHTML = 'No Summary provided'
+        }
+        if (!response.data.articles[i].urlToImage) {
+          newImage.src = 'http://i2.wp.com/www.4motiondarlington.org/wp-content/uploads/2013/06/No-image-found.jpg'
+        } else {
+          newImage.src = response.data.articles[i].urlToImage
+        }
+        if (response.data.articles[i].url) {
+          newLink.href = response.data.articles[i].url
+          newLink.innerHTML = 'Link to Article'
+        }
+        newThread.className = 'media my-4'
+        newHead.className = 'mt-0 mb-1'
+        newDiv.className = 'media-body'
+        newImage.className = 'mr-3 rounded'
+        newImage.style = 'max-height: 30%; max-width: 30%;'
+        newDiv.style = 'color: white;'
+        newHead.style = 'font-family: Roboto Condensed, sans-serif;'
+        newText.style = 'font-family: Playfair Display, serif;'
+        newDiv.appendChild(newHead)
+        newDiv.appendChild(newText)
+        newDiv.appendChild(newLink)
+        newThread.appendChild(newImage)
+        newThread.appendChild(newDiv)
+        list.appendChild(newThread)
+        list.appendChild(document.createElement('br'))
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
 
   const addFilter = (userInfomation2) =>{
     return  axios({
@@ -40,13 +98,25 @@ document.addEventListener('DOMContentLoaded', () => {
         filterArray = userInfo.filters
         console.log(filterArray)
         if (!checkLoggedIn()) {
+          console.log(userInfo)
           document.querySelector('.username1').innerHTML = 'Login'
         } else {
           console.log(userInfo)
           document.querySelector('.username1').innerHTML = `${userInfo.username}`
         }
         for (var i = 0; i < filterArray.length; i++) {
-          console.log(filterArray[i])
+          let filter = document.createElement('span')
+          let filterList2 = document.querySelector('.filterList')
+          filter.innerHTML = filterArray[i]
+          filter.className = `badge filter badge-secondary mr-2`
+          filter.style = 'font-size: 16px;'
+          filter.addEventListener('click', (event) => {
+            console.log(event.target.innerHTML)
+            updateFeed({
+              filter: event.target.innerHTML
+            })
+          })
+          filterList2.appendChild(filter)
         }
       })
     })
@@ -55,97 +125,24 @@ getUsername()
 
 const list = document.querySelector('.list')
 
-const updateFeed = (userInfomation3) =>{
-  axios({
-    method: 'post',
-    url: '/news',
-    data: userInfomation3
-  })
-  .then((response) => {
-    for (var i = 0; i < response.data.articles.length; i++) {
-      let newThread = document.createElement('li')
-      let newImage = document.createElement('img')
-      let newDiv = document.createElement('div')
-      let newHead = document.createElement('h5')
-      let newText = document.createElement('p')
-      let newLink = document.createElement('a')
-      if (response.data.articles[i].title) {
-        newHead.innerHTML = response.data.articles[i].title
-      } else {
-        newHead.innerHTML = 'No Headline provided'
-      }
-      if (response.data.articles[i].author) {
-        newHead.innerHTML += `, Created by: ${response.data.articles[i].author}`
-      }
-      if (response.data.articles[i].content) {
-        newText.innerHTML = response.data.articles[i].content
-      } else {
-        newText.innerHTML = 'No Summary provided'
-      }
-      if (!response.data.articles[i].urlToImage) {
-        newImage.src = 'http://i2.wp.com/www.4motiondarlington.org/wp-content/uploads/2013/06/No-image-found.jpg'
-      } else {
-        newImage.src = response.data.articles[i].urlToImage
-      }
-      if (response.data.articles[i].url) {
-        newLink.href = response.data.articles[i].url
-        newLink.innerHTML = 'Link to Article'
-      }
-      newThread.className = 'media my-4'
-      newHead.className = 'mt-0 mb-1'
-      newDiv.className = 'media-body'
-      newImage.className = 'mr-3 rounded'
-      newImage.style = 'max-height: 30%; max-width: 30%;'
-      newDiv.style = 'color: white;'
-      newHead.style = 'font-family: Roboto Condensed, sans-serif;'
-      newText.style = 'font-family: Playfair Display, serif;'
-      newDiv.appendChild(newHead)
-      newDiv.appendChild(newText)
-      newDiv.appendChild(newLink)
-      newThread.appendChild(newImage)
-      newThread.appendChild(newDiv)
-      list.appendChild(newThread)
-      list.appendChild(document.createElement('br'))
-    }
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-}
 
 updateFeed({
-  filter: 'news'
+  filter: 'apple'
 })
-
-const addFilter = (userInfo) =>{
-  return  axios({
-      method: 'post',
-      url: '/filters',
-      data: userInfo
-    })
-}
-
-const getUserFilters = (userInfo) =>{
-  return  axios({
-      method: 'get',
-      url: `/users/${userInfo.user_id}`
-    })
-}
-
-const checkLoggedIn = () =>{
-    return axios({
-        method: 'get',
-        url: '/login'
-    })
-}
-
+// let filter = document.querySelectorAll('.filter')
+// console.log(filter)
+//   filter.addEventListener('click', () => {
+//     console.log(event.target)
+//   })
   document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault()
     const userSearch = document.querySelector('.searchBar')
     if (userSearch.value === '') {
       userSearch.placeholder = 'Please enter a Filter'
     } else {
+      let filterlist3 = document.querySelector('.filterList')
       list.innerHTML = ''
+      filterlist3.innerHTML = ''
       userInfo.filter = userSearch.value
       checkLoggedIn()
       .then(result=>{
@@ -153,9 +150,8 @@ const checkLoggedIn = () =>{
           addFilter(userInfo)
           .then((response) => {
             userInfo['user_id'] = response.data.user_id
-            getUserFilters(userInfo)
+            getUsername(userInfo)
             .then(filterInfo=>{
-              console.log('getUserFilters',filterInfo.data)
               userInfo = filterInfo.data
             })
             updateFeed(userInfo)
