fb76a6265bf395f09c706c46e4512b8f54a999ee
Marc <marc.wilk@colorado.edu>
2018-12-18 15:32:39 -0700

    latest code

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -26,35 +26,36 @@ document.addEventListener('DOMContentLoaded', function () {
           window.location.href = 'login.html'
         }
       })
-  }) //End of DOM Content
-
-  function changePassword (id, newPassword) {
-    console.log(id)
-    return axios({
-      method: 'patch',
-      url: `/users/${id}`,
-      data: {
-        password: newPassword
-      }
-    })
-  }
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const oldPassword = document.getElementById('exampleInputPassword1')
-    const newPassword = document.getElementById('exampleInputPassword2')
-    const confirmNewPassword = document.querySelector('#confirmNewPassword')
-
-    confirmNewPassword.addEventListener('click', () => {
-      checkLoggedIn()
-        .then(result => {
-          if (result) {
-            console.log('test')
-            changePassword(result.data.user_id, newPassword)
-            window.location.href = 'settings.html'
-            axios.patch('/settings')
-          } else {
-            window.location.href = 'login.html'
-          }
-        })
-    })
   })
+}) //End of DOM Content
+
+function changePassword (id, newPassword) {
+  console.log(id)
+  return axios({
+    method: 'patch',
+    url: `/users/${id}`,
+    data: {
+      password: newPassword
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const oldPassword = document.getElementById('exampleInputPassword1')
+  const newPassword = document.getElementById('exampleInputPassword2')
+  const confirmNewPassword = document.querySelector('#confirmNewPassword')
+
+  confirmNewPassword.addEventListener('click', () => {
+    checkLoggedIn()
+      .then(result => {
+        if (result) {
+          console.log('test')
+          changePassword(result.data.user_id, newPassword)
+          window.location.href = 'settings.html'
+          axios.patch('/settings')
+        } else {
+          window.location.href = 'login.html'
+        }
+      })
+  })
+})
