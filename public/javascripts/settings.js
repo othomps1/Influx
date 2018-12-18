ff1b823ba4eb6e0e33c2d287d0aab1f3dbc902e8
Owen Thompson <othomps1986@gmail.com>
2018-12-18 14:45:27 -0700

    Updates

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.querySelector('button')
-  button.addEventListener('submit', (event) => {
-    event.preventDefault()
-    let user = {}
-    user.email = button.email.value
-    user.password = button.password.value
-    axios.delete('/users/:id')
+const checkLoggedIn = () =>{
+  return axios({
+    method: 'get',
+    url: '/login'
   })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const newPassword = document.getElementById('exampleInputEmail1')
 })
